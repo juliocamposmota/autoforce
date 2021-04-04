1f9c7b86f52cd4e7feee4dd7d17cc0076a8c1cdc
Júlio Mota <juliomotacampos@gmail.com>
2021-04-04 12:49:10 -0300

    Abrir sub-menu-itens concluído

--- parteII/js/main.js
+++ parteII/js/main.js
@@ -12,24 +12,35 @@ menuBtn.addEventListener('click', () => {
   }
 });
 
-let menuItem = document.querySelector('.menu-item');
-let subMenuItem = document.querySelector('.sub-menu');
-let arrow = document.querySelector('.arrow-icon');
+let menuItens = document.querySelectorAll('.menu-item');
+let subMenuItens = document.querySelectorAll('.sub-menu');
+let arrows = document.querySelectorAll('.arrow-icon');
+
 let menuItemOpen = false;
 
-menuItem.addEventListener('click', () => {
-  if(!menuItemOpen) {
-    menuItem.classList.add('open');
-    subMenuItem.classList.add('open');
-    arrow.classList.add('open');
-    menuItemOpen = true;
+for (let index = 0; index < menuItens.length; index += 1) {
+  if (!subMenuItens[index]) {
+    break;
   } else {
-    menuItem.classList.remove('open');
-    subMenuItem.classList.remove('open');
-    arrow.classList.remove('open');
-    menuItemOpen = false;
+    menuItens[index].addEventListener('click', () => {
+      if(!menuItemOpen) {
+        menuItens[index].classList.add('open');
+        subMenuItens[index].classList.add('open');
+        arrows[index].classList.add('open');
+        
+        menuItemOpen = true;
+      } else {
+        menuItens[index].classList.remove('open');
+        subMenuItens[index].classList.remove('open');
+        arrows[index].classList.remove('open');
+        
+        menuItemOpen = false;
+      }
+    });
   }
-});
+}
+
+
 
 
 // function mouseOver() {
